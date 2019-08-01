896689fda69c68276262fb3dd0f63ea8a2f8c7cc
shashank shankar <shashank.shankar@cura.tech>
2019-08-01 21:55:02 +0530

    New Source check-in

---
+++ src/main/java/com/talfinder/assessment/MergeStrings.java
@@ -0,0 +1,23 @@
+package com.talfinder.assessment;
+
+public class MergeStrings {
+
+    public static String merge(String first, String second) {
+        /*
+         * NOTE:
+         * 1. Write your solution within this method
+         * 2. Do not make changes to the Class Name, Method Name or Method Signature
+         * 3. Use the Run-Java command to run main method
+         * 4. Use the BuildAndRunTests command to evaluate your solution
+         */
+        return "asbtcuvwx";
+    }
+
+    public static void main(String[] args) {
+        String first = "abc";
+        String second = "stuvwx";
+
+        System.out.println("Merged String : " + merge(first, second));
+    }
+
+}
---
+++ src/test/java/com/talfinder/assessment/MergeStringsTest.java
@@ -0,0 +1,45 @@
+package com.talfinder.assessment;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class MergeStringsTest {
+
+    private String first;
+    private String second;
+    private String expectedResult;
+
+    public MergeStringsTest(String first, String second, String expectedResult) {
+        this.first = first;
+        this.second = second;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameterized.Parameters(name = "{index}: Test with First-String={0}, Second-String ={1}, Expected-Result is:{2} ")
+    public static Collection<Object[]> input() {
+        return Arrays.asList(new Object[][]{
+                {"abc", "stuvwx", "asbtcuvwx"},
+                {"stuvwx", "abc", "satbucvwx"},
+                {"true", "lies", "tlriuees"},
+                {"ma te ria l", " de sign ", "m ad et es irgina  l"},
+                {" ch e ck", "l is t", " lc hi se  tck"},
+                {"", "", ""},
+                {"trial", "", "trial"},
+                {"", "over", "over"},
+                {null, null, ""},
+                {"game", null, "game"},
+                {null, "plan", "plan"}
+        });
+    }
+
+    @Test
+    public void testMergeStrings() {
+        Assert.assertEquals(expectedResult, MergeStrings.merge(first, second));
+    }
+}
